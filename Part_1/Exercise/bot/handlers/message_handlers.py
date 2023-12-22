9aca1081eeed2c504449e1ec96e8c6192d5dfc7e
blenassefa2 <blenassefa59@yahoo.com>
2023-12-22 12:01:13 +0300

    (Blen.setup): edit the message handler for part_1

--- Part_1/Exercise/bot/handlers/message_handlers.py
+++ Part_1/Exercise/bot/handlers/message_handlers.py
@@ -9,4 +9,4 @@ message_router = Router()
 
 @message_router.message(Command('start'))
 async def start_handler(message: types.Message):
-    await message.reply("Hello Welcome to Telegram Bot Development Phase")
+    await message.answer("Hello Welcome to Telegram Bot Development Phase")
