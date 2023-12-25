b718c9398a0650eac56470299c917c9d6ecc8841
fasika <fasikafikadu38@gmail.com>
2023-12-25 11:04:24 +0300

    [add] chat and help commands added

--- Part_1/Fasika/bot/handlers/message_handlers.py
+++ Part_1/Fasika/bot/handlers/message_handlers.py
@@ -1,6 +1,6 @@
 from aiogram.filters import Command
 from aiogram import Router, types 
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton
 
 # from bot.config import BotConfig
 
@@ -10,3 +10,14 @@ message_router = Router()
 @message_router.message(Command('start'))
 async def start_handler(message: types.Message):
     await message.answer("Hello Welcome to Telegram Bot Development Phase")
+
+@message_router.message(Command('help'))
+async def help_handler(message: types.Message):
+    await message.answer("This is a help message. You can use /start, /help, and /chat commands.")
+
+
+@message_router.message(Command('chat'))
+async def chat_handler(message: types.Message):
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton("Open Chat", url="https://t.me/fasilfm"))
+    await message.answer("Click the button below to open the chat:", reply_markup=keyboard)
