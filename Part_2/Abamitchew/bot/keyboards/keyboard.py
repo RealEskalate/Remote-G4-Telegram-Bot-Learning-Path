a033b88f4795bcc316e44eb79b21066dc6c98c7c
John Doe <johndoe@email.com>
2023-12-25 05:36:53 -0800

    feat: Register keyboard

--- Part_2/Abamitchew/bot/keyboards/keyboard.py
+++ Part_2/Abamitchew/bot/keyboards/keyboard.py
@@ -36,3 +36,31 @@ second_inline_keyboard = InlineKeyboardMarkup(
     ]
 )
 
+register_buttons_reply_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Register phone"),
+            KeyboardButton(text="Register location")
+        ],
+        [
+            KeyboardButton(text="Custom Button 1"),
+            KeyboardButton(text="Custom Button 2")
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    selective=True
+)
+
+# Inline keyboard with Register phone and custom buttons
+register_buttons_inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Register phone", callback_data="register_phone"),
+            InlineKeyboardButton(text="Custom Button 1", callback_data="custom_button_1")
+        ],
+        [
+            InlineKeyboardButton(text="Custom Button 2", callback_data="custom_button_2")
+        ]
+    ]
+)
