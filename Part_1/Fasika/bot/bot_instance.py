4d8b43a05ebb350d2881d0a106aac44ec4d40c5c
fasika <fasikafikadu38@gmail.com>
2023-12-25 10:49:56 +0300

    [add] new bot token added

---
+++ Part_1/Fasika/bot/bot_instance.py
@@ -0,0 +1,18 @@
+import os
+from aiogram import Bot, types
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# For hosting on pythonanywhere use the following commented code
+# from aiogram.client.session.aiohttp import AiohttpSession
+# bot = Bot(
+#     token=TOKEN,
+#     parse_mode='HTML',
+#     session=AiohttpSession(proxy='http://proxy.server:3128')
+# )
+
+bot = Bot(
+    token=os.environ.get("TOKEN"),
+    parse_mode='HTML'
+)
