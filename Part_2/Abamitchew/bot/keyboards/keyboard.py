6f8bb5f2aeaee2e7ea049ff451613ef890c69ad4
John Doe <johndoe@email.com>
2023-12-25 05:31:11 -0800

    feat: created my folder

---
+++ Part_2/Abamitchew/bot/keyboards/keyboard.py
@@ -0,0 +1,38 @@
+
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton, InlineKeyboardButton
+
+first_reply_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="👋 Register!")
+        ],
+        [
+            KeyboardButton(text="ℹ About Me")
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder="dtrial",
+    selective=True
+)
+
+
+first_inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="👋 Register!", url="https://youtu.be/CBJiJcgmDmM"),
+            InlineKeyboardButton(text="ℹ About Me", url="https://youtu.be/CBJiJcgmDmM")
+        ]
+    ]
+)
+
+# inline keyboard with callback
+second_inline_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="action 1", callback_data="action_1"),
+            InlineKeyboardButton(text="action 2", callback_data="action_2")
+        ]
+    ]
+)
+
