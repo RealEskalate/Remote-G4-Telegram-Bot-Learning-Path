89f86656d2c2736895650fa6b8119860b4adce47
Kibreab Aklilu <Akkeba37@gmail.com>
2023-12-26 19:54:26 +0300

    Implement the bot functionality

--- Part_2/Kibreab/bot/callbacks/callback.py
+++ Part_2/Kibreab/bot/callbacks/callback.py
@@ -1,15 +1,15 @@
-from aiogram.filters import Command
-from aiogram import Router, types 
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import Router, types
 
 
 callback_router = Router()
 
 
-@callback_router.callback_query(lambda c: c.data.startswith("action_1"))
-async def process_callback_respond_to_action1(callback_query: types.CallbackQuery):
+@callback_router.callback_query(lambda c: c.data == "register_phone")
+async def process_custom_inline_button_1(callback_query: types.CallbackQuery):
+    await callback_query.answer("You clicked on Custom register phone button")
     await callback_query.message.answer(f"you picked {callback_query.data}")
 
-@callback_router.callback_query(lambda c: c.data.startswith("action_2"))
-async def process_callback_respond_to_action1(callback_query: types.CallbackQuery):
-    await callback_query.message.answer(f"you picked {callback_query.data}")
+
+@callback_router.callback_query(lambda c: c.data == "share_location")
+async def process_custom_inline_button_2(callback_query: types.CallbackQuery):
+    await callback_query.answer("You clicked on register location button")
--- Part_2/Kibreab/bot/handlers/message_handlers.py
+++ Part_2/Kibreab/bot/handlers/message_handlers.py
@@ -1,20 +1,45 @@
+import phonenumbers
+from phonenumbers import NumberParseException
+from aiogram import Router, types
 from aiogram.filters import Command
-from aiogram import Router, types 
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.fsm.context import FSMContext
 
 from ..keyboards import keyboard
 
 message_router = Router()
 
-@message_router.message(Command('start_with_reply_keyboard'))
+
+@message_router.message(Command("start"))
 async def start_handler(message: types.Message):
-    await message.answer("Hello Welcome to Telegram Bot Development Phase", reply_markup=keyboard.first_reply_keyboard)
+    await message.answer(
+        "Hello! Welcome to demo bot 2",
+        reply_markup=keyboard.start_reply_keyboard,
+    )
 
 
-@message_router.message(Command('start_with_inline_keyboard'))
-async def start_handler(message: types.Message):
-    await message.answer("Hello Welcome to Telegram Bot Development Phase", reply_markup=keyboard.first_inline_keyboard)
+@message_router.message(lambda message: message.text == "👋 Register!")
+async def register_option_handler(message: types.Message, state: FSMContext):
+    await state.set_state("waiting_for_phone")
+    await message.answer("Please choose an action:", reply_markup=keyboard.register_reply_keyboard)
+    await message.reply("Please choose an action", reply_markup=keyboard.register_inline_keyboard)
 
-@message_router.message(Command('see_callback_buttons'))
-async def start_handler(message: types.Message):
-    await message.answer("Hello Welcome to Telegram Bot Development Phase", reply_markup=keyboard.second_inline_keyboard)
+
+@message_router.message(lambda message: message.text == "ℹ About Me")
+async def about_me_option_handler(message: types.Message):
+    await message.answer("You selected 'About Me'. What would you like to do next?")
+
+
+@message_router.message()
+async def text_message_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+
+    if current_state == "waiting_for_phone":
+        try:
+            phone_number = phonenumbers.parse(message.text, None)
+            if phonenumbers.is_valid_number(phone_number):
+                await message.answer(f"Thank you for sharing your phone number: {phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL)}")
+                await state.reset_state()
+            else:
+                await message.answer("Invalid phone number format. Please enter a valid phone number.")
+        except NumberParseException:
+            await message.answer("Invalid phone number format. Please enter a valid phone number.")
