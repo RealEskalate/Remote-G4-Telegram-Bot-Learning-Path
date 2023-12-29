019d7f92c64c4acf4434ae27c5df9b6b11ab94c4
fasika <fasikafikadu38@gmail.com>
2023-12-29 21:39:55 +0300

    [add] database configuration added

--- Part_3/Fasika/bot/bot_instance.py
+++ Part_3/Fasika/bot/bot_instance.py
@@ -5,15 +5,15 @@ from aiogram import Bot, types
 from dotenv import load_dotenv
 load_dotenv()
 
-# # For hosting on pythonanywhere use the following commented code
-# from aiogram.client.session.aiohttp import AiohttpSession
-# bot = Bot(
-#     token=os.environ.get("TOKEN_API"),
-#     parse_mode='HTML',
-#     session=AiohttpSession(proxy='http://proxy.server:3128')
-# )
-
+# For hosting on pythonanywhere use the following commented code
+from aiogram.client.session.aiohttp import AiohttpSession
 bot = Bot(
     token=os.environ.get("TOKEN_API"),
-    parse_mode='HTML'
+    parse_mode='HTML',
+    session=AiohttpSession(proxy='http://proxy.server:3128')
 )
+
+# bot = Bot(
+#     token=os.environ.get("TOKEN_API"),
+#     parse_mode='HTML'
+# )
