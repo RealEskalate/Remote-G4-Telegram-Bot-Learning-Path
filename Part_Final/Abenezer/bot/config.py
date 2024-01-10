83fe1678f76ee3c154e207e9bca809b963759617
Abenezer0923 <abnew0923@gmail.com>
2024-01-11 00:27:16 +0300

    task

--- Part_Final/Abenezer/bot/config.py
+++ Part_Final/Abenezer/bot/config.py
@@ -12,8 +12,8 @@ config_dir = Path(__file__).parent.parent.resolve() / "config"
 # config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = "6837368665:AAG9KFtKsCONj8oQuUfXE8GE4pMRxurR8MM"
-openai_api_key = "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"
+telegram_token = "Token"
+openai_api_key = "api_key"
 openai_api_base = None
 allowed_telegram_usernames = []
 new_dialog_timeout = 600
@@ -21,7 +21,7 @@ enable_message_streaming = True
 return_n_generated_images = 1
 image_size = "512x512" 
 n_chat_modes_per_page = 5
-mongodb_uri = "mongodb+srv://Abenezer:0923931869@cluster0.1ciku1g.mongodb.net/BotAI"
+mongodb_uri = ""
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
